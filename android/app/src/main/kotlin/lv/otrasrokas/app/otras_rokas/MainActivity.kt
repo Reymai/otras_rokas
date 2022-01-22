a230338f9d6ac11a7422cff11ddff85f205973ce
Reymai <ricosigla@gmail.com>
2022-01-23 00:14:26 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/lv/otrasrokas/app/otras_rokas/MainActivity.kt
@@ -0,0 +1,6 @@
+package lv.otrasrokas.app.otras_rokas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatefulWidget {
+  const MainApp({Key? key}) : super(key: key);
+
+  @override
+  _MainAppState createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
+  Widget build(BuildContext context) => Container();
+}
